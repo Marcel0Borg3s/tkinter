2e82a321310001cbecfdfcdf8e3daa675056d5a2
marcelo_borges <marcelosbgs@gmail.com>
2025-03-19 20:20:16 -0300

    Correção visual na posição das linhas

--- criando_janelas.py
+++ criando_janelas.py
@@ -30,27 +30,27 @@ frame.grid(column=0, row=0)
 Label(frame, text="    Digite os dados abaixo para saber seu IMC:    ", font=("Arial", 12), pady=5).grid(column=1, row=0, columnspan=2) 
 Label(frame, text="    Sempre utilize ponto '.' para calcular", font=("Arial", 10), pady=10).grid(column=1, row=1, columnspan=2) 
 
-Label(frame, text="Qual é o seu peso? (kg) ").grid(column=1, row=2)
+Label(frame, text="Qual é o seu peso? (kg) ").grid(column=1, sticky="w", row=4)
 # Adicionando o input:
 peso_kg = Entry(frame)
-peso_kg.grid(column=2, row=2) 
+peso_kg.grid(column=2, row=4) 
 
-Label(frame, text="Qual é o sua altura?").grid(column=1, sticky="w", row=3)
+Label(frame, text="Qual é o sua altura?").grid(column=1, sticky="w", row=5)
 # Adicionando o input:
 altura_cm = Entry(frame)
-altura_cm.grid(column=2, row=3)
+altura_cm.grid(column=2, row=5)
 
-Label(frame, text="Qual é sua idade? ").grid(column=1, sticky="w", row=4)
+Label(frame, text="Qual é sua idade? ").grid(column=1, sticky="w", row=6)
 # Adicionando o input:
 idade = Entry(frame)
-idade.grid(column=2, row=4)
+idade.grid(column=2, row=6)
 
 # Botão para criar o calculo
-Button(frame, text="Calcular", pady=5, command=calcular).grid(column=1, row=6, columnspan=2)
+Button(frame, text="Calcular", padx= 10, pady=5, command=calcular).grid(column=1, row=12, columnspan=2)
 
 # Rótulo para exibir o resultado
-resultado_label = Label(frame, text="", font=("Arial", 12), pady=10)
-resultado_label.grid(column=1, row=5, columnspan=2)
+resultado_label = Label(frame, text="", font=("Arial", 12), pady=12)
+resultado_label.grid(column=1, row=15, columnspan=2)
 
 
 janela.mainloop()
