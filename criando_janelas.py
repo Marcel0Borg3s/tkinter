9a61d04a3fddc42a21af7af4b64de1575e5b6203
marcelo_borges <marcelosbgs@gmail.com>
2025-03-19 20:04:06 -0300

    Mensagem do commit

--- criando_janelas.py
+++ criando_janelas.py
@@ -1,12 +1,56 @@
 import tkinter as tk
-from tkinter import Label
+from tkinter import Label, Frame, Entry, Button
+from imc_calc import calcular_imc
 
+
+def calcular():
+    try:
+        peso = float(peso_kg.get())
+        altura = float(altura_cm.get()) 
+        idade_valor = int(idade.get())
+
+        resultado = calcular_imc(peso, altura, idade_valor)
+        resultado_label.config(text=resultado)
+
+    except ValueError:
+        resultado_label.config(text="Por favor, insira valores válidos.")
+
+# Criando a janela
 janela = tk.Tk()
 janela.title("Calculadora de IMC") # Cria o título da janela
+
 
 # Conteúdo da janela
 
 # Label: onde Label (rótulo/etiqeta), e o grid vai posicionar na coluna e linha indicada 
-Label(janela, text="    Digite os dados abaixo para saber seu IMC:    ", font=("Arial", 12)).grid(column=2, row=2) 
+frame = Frame(janela, padx=10, pady=10)
+frame.grid(column=0, row=0) 
+
+# Pad x e y é para espaçar dentro da janela // colunmspan=2 é pq tem 2 colunas, para centralizar entre as 2
+Label(frame, text="    Digite os dados abaixo para saber seu IMC:    ", font=("Arial", 12), pady=5).grid(column=1, row=0, columnspan=2) 
+Label(frame, text="    Sempre utilize ponto '.' para calcular", font=("Arial", 10), pady=10).grid(column=1, row=1, columnspan=2) 
+
+Label(frame, text="Qual é o seu peso? (kg) ").grid(column=1, row=2)
+# Adicionando o input:
+peso_kg = Entry(frame)
+peso_kg.grid(column=2, row=2) 
+
+Label(frame, text="Qual é o sua altura?").grid(column=1, sticky="w", row=3)
+# Adicionando o input:
+altura_cm = Entry(frame)
+altura_cm.grid(column=2, row=3)
+
+Label(frame, text="Qual é sua idade? ").grid(column=1, sticky="w", row=4)
+# Adicionando o input:
+idade = Entry(frame)
+idade.grid(column=2, row=4)
+
+# Botão para criar o calculo
+Button(frame, text="Calcular", pady=5, command=calcular).grid(column=1, row=6, columnspan=2)
+
+# Rótulo para exibir o resultado
+resultado_label = Label(frame, text="", font=("Arial", 12), pady=10)
+resultado_label.grid(column=1, row=5, columnspan=2)
+
 
 janela.mainloop()
